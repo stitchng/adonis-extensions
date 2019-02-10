b5fe67cfb80ba054986d968cbe024e849616f570
StitchNG <stitchnig@gmail.com>
2019-02-10 21:47:57 +0100

    Create ValidationExtensionProvider.js

---
+++ providers/ValidationExtensionProvider.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class ValidationExtensionProvider extends ServiceProvider {
+
+  /**
+	 * Register namespaces to the IoC container
+	 *
+	 * @method register
+	 *
+	 * @return {void}
+	 */
+	register() {
+		//....
+	}
+
+	/**
+	 * Attach context getter when all providers have
+	 * been registered
+	 *
+	 * @method boot
+	 *
+	 * @return {void}
+	 */
+	boot() {
+  
+  }
+}
+
+module.exports = ValidationExtensionProvider
