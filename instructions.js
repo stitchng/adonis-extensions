b4d21984cdf1cf83ccc291810b90e9f9d729559a
StitchNG <stitchnig@gmail.com>
2019-02-10 18:53:08 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * adonis-extensions
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ *
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com> | Aziz Abdul <>
+ */
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  await cli.makeConfig('extension.js', path.join(__dirname, './config/extension.js'))
+    .catch((e) => {})
+    
+  cli.command.completed('create', 'config/extension.js')
+}
