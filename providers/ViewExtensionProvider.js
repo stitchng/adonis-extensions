d25f4f6c9da37ae4d96209a1178f73c8bbfd21f3
StitchNG <stitchnig@gmail.com>
2019-03-07 16:31:35 +0100

    Update ViewExtensionProvider.js

--- providers/ViewExtensionProvider.js
+++ providers/ViewExtensionProvider.js
@@ -27,11 +27,38 @@ class ViewExtensionProvider extends ServiceProvider {
 
 		const View = this.app.use('Adonis/Src/View')
     
-    View.global('count', function (collection = []) {
+    View.global('toImage', function (source, attributes = {}) {
+      let assetsUrl = this.resolve('assetsUrl')
+      let { longdesc, name, id, alt } = attributes
+      return this.safe(`<img src="${assetsUrl(source)}" ` + (id?`id="{id}" `:'') + (alt?`alt="${alt}" `:'') + (name?`name="${name}" `:'') + (longdesc?`longdesc="${longdesc}" `:'') `/>`)
+    })
+    
+    View.global('toFrame', function(source, attributes = {}){
+    let assetsUrl = this.resolve('assetsUrl')
+      let { frameborder, scrolling, name, id, alt } = attributes
+      return this.safe(`<iframe src="${assetsUrl(source)}" ` + (id?`id="{id}" `:'') + (scrolling?`scrolling="${scrolling}" `:'') + (name?`name="${name}" `:'') + (frameborder?`frameborder="${frameborder}" `:'') `>`)
+    })
+    
+    View.global('prerender', function (routeNameOrAction, routeParams = {}) {
+      let route = this.resolve('route')
+      let origin = this.resolve('origin')
+      return this.safe(`<link rel="prerender" href="${origin}${route(routeNameOrAction, routeParams)}"/>`)
+    })
+    
+    View.global('dnsPrefetch', function (origin, pathName) {
+      return this.safe(`<link rel="dns-prefetch" href="${origin}${pathName}"/>`)
+    })
+    
+    View.global('favIcon', function (url) {
+      let assetsUrl = this.resolve('assetsUrl')
+      return this.safe(`<link rel="shortcut icon" href="${assetsUrl(url)}" type=""/>`)
+    })
+    
+    View.global('end', function (collection = []) {
         if(Array.isArray(collection)){
-            return collection.length
+            return collection.pop()
         }
-        return -1
+        return null
     })
 
     
