48dee6e913040ebc10584465ef8a3674bad807b7
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2021-12-11 08:35:24 +0100

    Update src/Middleware/StreamResponseBody.js

--- src/Middleware/StreamResponseBody.js
+++ src/Middleware/StreamResponseBody.js
@@ -1,13 +1,19 @@
 'use strict'
 
+const nodeReq = require('node-req')
+
 class StreamResponseBody {
   constructor (Config) {
     this.chunkedResponse = Config.get('extension.chunkedResponse')
   }
 
-  async handle ({ response }, next) {
+  async handle ({ request, response }, next) {
+    const charsets = request.header('Accept-Charset', null)
     response.transform(
-      'utf8',
+      charsets !== null ? nodeReq.charset(
+        response.request,
+        ['utf-8', 'utf-7', 'iso-8859-1', 'us-ascii', 'base64']
+      ) : 'utf-8',
       this.chunkedResponse
     )
 
