f1c89d23bc9ed2a9c9e816b12b070e48fa2cbf0f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2021-12-13 22:42:01 +0100

    Update providers/ResponseExtensionProvider.js

--- providers/ResponseExtensionProvider.js
+++ providers/ResponseExtensionProvider.js
@@ -177,7 +177,7 @@ class ResponseExtensionProvider extends ServiceProvider {
         }
 
         entityBody.stream(that.response, readStream).catch(
-          error => that.abortIf(true, error.code === 'ENOENT' ? 404 : 500, `Something unexpected happened: ${error.message}`)
+          error => that.abortIf(error !== null, error.code === 'ENOENT' ? 404 : 500, `Something unexpected happened: ${error.message}`)
         )
       })
 
