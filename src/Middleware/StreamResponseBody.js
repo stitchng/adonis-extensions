0831bec7efbbb753690d28f91940007606668794
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2021-12-14 01:00:13 +0100

    Update src/Middleware/StreamResponseBody.js

--- src/Middleware/StreamResponseBody.js
+++ src/Middleware/StreamResponseBody.js
@@ -7,15 +7,29 @@ class StreamResponseBody {
     this.chunkedResponse = Config.get('extension.chunkedResponse')
   }
 
-  async handle ({ request, response }, next) {
-    const charsets = request.header('Accept-Charset', null)
-    response.transform(
-      charsets !== null ? nodeReq.charset(
-        response.request,
-        ['utf-8', 'utf-7', 'iso-8859-1', 'us-ascii', 'base64']
-      ) : 'utf-8',
-      this.chunkedResponse
+  async handle ({ request, response }, next, [ chunked, multipart ]) {
+    const accept = request.header('Accept', '')
+    const charsets = request.header(
+      'Accept-Charset',
+      accept.replace(/\b; charset=(.*)$/, function(pattern, substring) {
+        return typeof pattern === 'string' ? substring : null
+      })
     )
+
+    response.transform(
+      nodeReq.charset(
+        response.request,
+        charsets !== null
+          ? charsets.split(', ')
+          : ['utf-8', 'utf-7', 'iso-8859-1', 'us-ascii']
+      ),
+     {
+       chunked: (chunked === 'chunked' || multipart === 'chunked' || this.chunkedResponse),
+       multipart: (chunked === 'multipart' || multipart === 'multipart')
+    )
+
+    /* @HINT: send whitespace chars to client so it (client) doesn't attempt to close connection */
+    process.nextTick(() => response.sendToStream('                  '))
 
     await next()
   }
